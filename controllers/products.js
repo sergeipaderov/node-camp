9327ef72d8b980b06b127475916b644a8014b303
Serg <sergeipaderov@gmail.com>
2020-03-25 15:43:11 -0300

    added product-model + local data

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = []
+const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
@@ -11,19 +11,20 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({
-        title: (req.body.title !== '' ? req.body.title : 'New Product')
-    })
+    const product = new Product(req.body.title)
+    product.save()
     res.redirect('/')
 }
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    Product.fetchAll(products => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        })
     })
 }
---
+++ models/product.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const path = require('path')
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title ? title : 'New Product'
+    }
+
+    save() {
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+        fs.readFile(p, (err, fileContent) => {
+            let products = []
+
+            if (!err) {
+                products = JSON.parse(fileContent)
+            }
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        })
+    }
+
+    static fetchAll(cb) {
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([])
+            }
+
+            cb(JSON.parse(fileContent))
+        })
+    }
+}
