b573dc1b791ea8a01bfb12b04ff98bff33d34796
Serg <sergeipaderov@gmail.com>
2020-03-26 21:42:05 -0300

    refactor

--- models/product.js
+++ models/product.js
@@ -14,8 +14,11 @@ const getProductsFromFile = cb => {
 }
 
 module.exports = class Product {
-    constructor(title) {
+    constructor(title, imageUrl, description, price) {
         this.title = title ? title : 'New Product'
+        this.imageUrl = imageUrl ? imageUrl : 'https://cdn.pixabay.com/photo/2016/03/09/15/21/glasses-1246611_1280.jpg'
+        this.description = description ? description : ''
+        this.price = price ? price : ''
     }
 
     save() {
