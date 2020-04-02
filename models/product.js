503d34ef3f8abb4deff2d9dd1b8d49509b6c469d
Serg <sergeipaderov@gmail.com>
2020-04-02 19:26:42 -0300

    refactored

--- models/product.js
+++ models/product.js
@@ -17,14 +17,14 @@ module.exports = class Product {
     constructor(title, imageUrl, description, price) {
         this.title = title ? title : 'New Product'
         this.imageUrl = imageUrl ? imageUrl : 'https://cdn.pixabay.com/photo/2016/03/09/15/21/glasses-1246611_1280.jpg'
-        this.description = description ? description : ''
-        this.price = price ? price : ''
+        this.description = description ? description : 'Some Description'
+        this.price = price ? price : 0
     }
 
     save() {
         getProductsFromFile(products => {
             products.push(this)
-            fs.writeFile(p, JSON.stringify(products), (err) => {
+            fs.writeFile(p, JSON.stringify(products), err => {
                 console.log(err)
             })
         })
