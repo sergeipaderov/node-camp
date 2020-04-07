989fcfa4ef051f07743ba155fe71da3924f02b97
Serg <sergeipaderov@gmail.com>
2020-04-07 18:03:09 -0300

    added cart model

--- models/product.js
+++ models/product.js
@@ -22,6 +22,7 @@ module.exports = class Product {
     }
 
     save() {
+        this.id = Math.random().toString()
         getProductsFromFile(products => {
             products.push(this)
             fs.writeFile(p, JSON.stringify(products), err => {
@@ -32,5 +33,12 @@ module.exports = class Product {
 
     static fetchAll(cb) {
         getProductsFromFile(cb)
+    }
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(prod => prod.id === id)
+            cb(product)
+        })
     }
 }
