eb55cdd0dfb2fc7d10e7a241f922696d879ef22b
Serg <sergeipaderov@gmail.com>
2020-04-27 14:14:26 -0300

    added edit of products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,59 +1,73 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/edit-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/add-product',
-        editing: false
-    })
-}
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false
+  });
+};
 
 exports.postAddProduct = (req, res, next) => {
-    const {
-        title,
-        imageUrl,
-        description,
-        price
-    } = req.body
+  const { title, imageUrl, price, description } = req.body;
 
-    const product = new Product(title, imageUrl, description, price)
-    product.save()
-    res.redirect('/')
-}
+  const product = new Product(null, title, imageUrl, price, description );
+  product.save();
+  res.redirect("/");
+};
 
 exports.getEditProduct = (req, res, next) => {
-    const editMode = req.query.edit
+  const editMode = req.query.edit;
 
-    if (!editMode) {
-        return res.redirect('/')
+  if (!editMode) {
+    return res.redirect("/");
+  }
+
+  const prodId = req.params.productId;
+
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect("/");
     }
 
-    const prodId = req.params.productId
-
-    Product.findById(prodId, product => {
-        if (!product) {
-            return res.redirect('/')
-        }
-
-        res.render('admin/edit-product', {
-            pageTitle: 'Edit Product',
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product
-        })
-    })
-}
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product
+    });
+  });
+};
 
 exports.postEditProduct = (req, res, next) => {
-    console.log('postEditProduct')
-}
+  const { productId, title, imageUrl, price, description } = req.body;
+
+  console.log(req.body)
+
+  const prodId = productId;
+  const updatedTitle = title;
+  const updatedImageUrl = imageUrl;
+  const updatedPrice = price;
+  const updatedDesc = description;
+
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedPrice,
+    updatedDesc
+  );
+  console.log(updatedProduct)
+  updatedProduct.save()
+  res.redirect('/admin/products')
+};
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products'
-        })
-    })
-}
+  Product.fetchAll(products => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products"
+    });
+  });
+};
--- models/product.js
+++ models/product.js
@@ -14,20 +14,31 @@ const getProductsFromFile = cb => {
 }
 
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
+    constructor(id, title, imageUrl, price, description) {
+        this.id = id
         this.title = title ? title : 'New Product'
         this.imageUrl = imageUrl ? imageUrl : 'https://cdn.pixabay.com/photo/2016/03/09/15/21/glasses-1246611_1280.jpg'
-        this.description = description ? description : 'Some Description'
         this.price = price ? price : 0
+        this.description = description ? description : 'Some Description'
     }
 
     save() {
-        this.id = Math.random().toString()
         getProductsFromFile(products => {
-            products.push(this)
-            fs.writeFile(p, JSON.stringify(products), err => {
-                console.log(err)
-            })
+            if (this.id) {
+                const existingProductIndex = products.findIndex(prod => prod.id === this.id)
+                
+                const updatedProducts = [...products]
+                updatedProducts[existingProductIndex] = this
+                fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+                    console.log(err)
+                })
+            } else {
+                this.id = Math.random().toString()
+                products.push(this)
+                fs.writeFile(p, JSON.stringify(products), err => {
+                    console.log(err)
+                })
+            }
         })
     }
 
